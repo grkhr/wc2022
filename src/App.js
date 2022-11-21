86cdbc9d87df9ec50ec9fb3d9167dfee2acbf5c6
grkhr <eduardgorkh@gmail.com>
2022-11-21 22:48:02 +0400

    фвыы

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef } from "react";
 
-function App() {
+import schedule from "./schedule.json";
+
+const Day = (day) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h2>{day.day}</h2> */}
+      <ul>
+        {day.matches.map((match) => (
+          <li key={match.ts}>
+            {formatTime(match.ts)} | {match.match}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+// format date, e.g. 2021-06-12 00:00:00 -> "12 June, Tuesday"
+const formatDate = (date) => {
+  const d = new Date(date);
+  const options1 = { weekday: "long" };
+  const s1 = d.toLocaleDateString("en-GB", options1);
+  const options2 = { month: "numeric", day: "numeric" };
+  const s2 = d.toLocaleDateString("en-GB", options2);
+  return `${s2}, ${s1}`;
+};
+
+const formatTime = (date) => {
+  const d = new Date(date);
+  const options = { hour: "numeric", minute: "numeric" };
+  return d.toLocaleTimeString("en-GB", options);
+};
+
+const ScrollDemo = () => {
+  const myRef = useRef(null);
+
+  const executeScroll = () => myRef.current.scrollIntoView();
+  // run this function from an event handler or an effect to execute scroll
+
+  return (
+    <>
+      <div ref={myRef}>Element to scroll to</div>
+      <button onClick={executeScroll}> Click to scroll </button>
+    </>
+  );
+};
+
+// find index of date in schedule which is closest to current date from below
+const findClosestDate = (schedule) => {
+  const now = new Date();
+  const dates = schedule.map((day) => new Date(day.date));
+  const closest = dates.reduce((prev, curr) =>
+    Math.abs(curr - now) < Math.abs(prev - now) ? curr : prev
+  );
+  return dates[dates.indexOf(closest) - 1];
+};
+
+
+const HeaderDate = (day) => {
+  const closest = findClosestDate(schedule).toLocaleDateString();
+  const current = new Date(day.date).toLocaleDateString();
+  if (closest === current) {
+    return (
+      // draw horizontal line
+      <div>
+        <hr />
+        <h2>{formatDate(day.date)}</h2>
+        <Day key={day.date} {...day} />
+        <hr />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{formatDate(day.date)}</h1> <Day key={day.date} {...day} />
+    </div>
+  );
+};
+
+const App = () => {
+  const myRef = useRef("scroll");
+  console.log("DATEE", findClosestDate(schedule));
+  const executeScroll = () => myRef.current.scrollIntoView();
+  // useEffect(() => {
+  //   myRef.current.scrollIntoView();
+  // }, []);
+
+  return (
+    <div>
+      <div>
+        {schedule.map((day) => (
+          <>
+            <HeaderDate {...day} />
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
